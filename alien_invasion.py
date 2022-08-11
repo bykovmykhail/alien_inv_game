206842d90bff36ce983cb1f7b84e882591f71d1b
bykovmykhail <bykovmykhail@gmail.com>
2022-08-11 17:49:04 +0300

    Add settings pack and gitignore

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,18 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Загальний клас, що керує ресурсами та поведінкою гри."""
 
     def __init__(self):
         """Ініціалізувати гру, створити ресурси гри"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1100, 710))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien invasion")
 
     def run_game(self):
@@ -18,6 +22,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Наново перемалювати екран на кожній ітерації циклу.        
+            self.screen.fill(self.settings.bg_color)
 
             # Показати осатнній намальований екран.
             pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    def __init__(self):
+        """Ініціалізувати налаштування гри"""
+        # Screen settings 
+        self.screen_width = 1100
+        self.screen_height = 710
+        self.bg_color = (51, 102, 255)
+        
